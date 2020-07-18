417d4287c75ebc2c980340d5fe67775e8fc2d3c8
Priyank <ppraja20@my.centennialcollege.ca>
2020-07-18 19:30:17 -0400

    Added Splash Screen and GUI

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    //Start Game
+    public void StartGame()
+    {
+        //Load the first level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Main Menu
+    public void MainMenu()
+    {
+        //Load Main menu
+        SceneManager.LoadScene(0);
+    }
+
+    //Quit Game
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
